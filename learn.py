cc5e35c156eac87b35a16c3361310cceb854db44
NightRain <48847328+Iamdreaming@users.noreply.github.com>
2020-03-15 17:00:08 +0800

    Update learn.py

--- learn.py
+++ learn.py
@@ -2,10 +2,11 @@
 
 import re 
 
+path = 'test1.c'
 #检查readlines()函数用法
 def test1():
-    file_path = 'test3.c'
-    fb = open(file_path,'r')
+    
+    fb = open(path,'r')
     words = fb.read()
         
     words = words.replace(' ','')
@@ -13,3 +14,16 @@ def test1():
     print("字符数为"+str(len(words)))
 
 test1()
+def words_count():
+    file_onject = open(path,'r')
+    context = file_onject.read()
+    words = context.split()
+    print('单词个数为'+ str(len(words)))
+words_count()
+def lines_count():
+    file_object = open(path,'r')
+    count = 0
+    for line in file_object.readlines():
+        count += 1
+    print('行数为'+ str(count))
+lines_count()
