39f4149ca37c4e56993f05a905fcf477e06edc75
NightRain <48847328+Iamdreaming@users.noreply.github.com>
2020-03-24 10:54:37 +0800

    完成

--- wc.py
+++ wc.py
@@ -1,8 +1,10 @@
 import os
 import re
+import time
 import tkinter
 from tkinter import ttk
 from tkinter import filedialog
+
 
 class text_count():
 
@@ -11,22 +13,26 @@ class text_count():
 
     def char_count(self):
         # 统计字符数 -c
-        file_object = open(self.path, 'r',encoding= 'utf-8')
+        file_object = open(self.path, 'r', encoding='utf-8')
         context = file_object.read()
+
+        # 将空格和换行符去掉
         context = context.replace(' ', '')
         context = context.replace('\n', '')
         string = "字符数为"+str(len(context))
         print(string)
-        return string
 
     def words_count(self):
         # 统计单词数 -w
-        file_object = open(self.path, 'r', encoding='UTF-8')
+        reg = re.compile(r'\w+')
+        file_object = open(self.path, 'r',encoding= 'utf-8')
         context = file_object.read()
         words = context.split()
+        for word in words:
+            if reg.search(word) == None:
+                words.remove(word)
         string = '单词个数为' + str(len(words))
         print(string)
-        return string
 
     def lines_count(self):
         # 统计行数 -l
@@ -36,7 +42,7 @@ class text_count():
             count += 1
         string = '行数为' + str(count)
         print(string)
-        return string
+
     def more_information(self):
         # 返回更复杂的数据（代码行 / 空行 / 注释行） -a
         file_object = open(self.path, 'r', encoding='UTF-8')
@@ -59,53 +65,66 @@ class text_count():
         string1 = '空行数为' + str(count[0])
         string2 = '注释行数为' + str(count[1])
         string3 = '代码行数为' + str(count[2])
-        string_all = [string1,string2,string3]
-        
-        print(string1)
-        print(string2)
-        print(string3)
+        string_all = [string1, string2, string3]
         return string_all
 
 
 class infor_windows(tkinter.Frame):
     def __init__(self, master):
+        # 将窗口分为三个部分，对应三个容器frame1,frame2,order_frame
         frame1 = tkinter.Frame(master)
         frame2 = tkinter.Frame(master)
         order_frame = tkinter.Frame(master)
-        frame1.grid(row = 0, column = 5)
-        frame2.grid(row = 4, column = 5)
-        order_frame.grid(row = 4, column = 12)
+        frame1.grid(row=0, column=5)
+        frame2.grid(row=4, column=5)
+        order_frame.grid(row=4, column=12)
+
+        # self.name获取文件名并显示在entry文本框
         self.name = tkinter.Variable()
-        self.entry = tkinter.Entry(frame1,textvariable = self.name)
-        self.entry.pack(side = 'left')
-        self.text = tkinter.Text(frame2, width = 40 ,height = 20)        
-        self.button1 = tkinter.Button(frame1,text = '选择',command = self.open_file)       
-        self.button1.pack(side = 'right')       
+        self.entry = tkinter.Entry(frame1, textvariable=self.name)
+        self.entry.pack(side='left')
+
+        # self.text显示文本内容
+        self.text = tkinter.Text(frame2, width=40, height=20)
+
+        # 文件选择按钮 self.select_button，调用open_fil()函数
+        self.select_button = tkinter.Button(
+            frame1, text='选择', command=self.open_file)
+        self.select_button.pack(side='right')
         self.text.pack()
 
-        self.c_button = tkinter.Button(order_frame,text = 'c命令',command = self.order_c)
+        # c命令按钮以及结果显示
+        self.c_button = tkinter.Button(
+            order_frame, text='c命令', command=self.order_c)
         self.get_c = tkinter.Variable()
-        self.show_c = tkinter.Entry(order_frame,textvariable = self.get_c)
+        self.show_c = tkinter.Entry(order_frame, textvariable=self.get_c)
         self.c_button.pack()
         self.show_c.pack()
 
-        self.w_button = tkinter.Button(order_frame,text = 'w命令',command = self.order_w)
+        #
+        self.w_button = tkinter.Button(
+            order_frame, text='w命令', command=self.order_w)
         self.get_w = tkinter.Variable()
-        self.show_w = tkinter.Entry(order_frame,textvariable = self.get_w)
+        self.show_w = tkinter.Entry(order_frame, textvariable=self.get_w)
         self.w_button.pack()
         self.show_w.pack()
 
-        self.l_button = tkinter.Button(order_frame,text = 'l命令',command = self.order_l)
+        #
+        self.l_button = tkinter.Button(
+            order_frame, text='l命令', command=self.order_l)
         self.get_l = tkinter.Variable()
-        self.show_l = tkinter.Entry(order_frame,textvariable = self.get_l)
+        self.show_l = tkinter.Entry(order_frame, textvariable=self.get_l)
         self.l_button.pack()
         self.show_l.pack()
 
-        self.a_button = tkinter.Button(order_frame,text = 'a命令',command = self.order_a)
-        self.show_a = tkinter.Text(order_frame,width = 20,height = 3)
+        #
+        self.a_button = tkinter.Button(
+            order_frame, text='a命令', command=self.order_a)
+        self.show_a = tkinter.Text(order_frame, width=20, height=3)
         self.a_button.pack()
         self.show_a.pack()
 
+    # 使用text_count类并传递参数，调用相应函数返回结果
     def order_c(self):
         count_c = text_count(self.file_name)
         self.get_c.set(count_c.char_count())
@@ -123,23 +142,25 @@ class infor_windows(tkinter.Frame):
         count_a = text_count(self.file_name)
         strings = count_a.more_information()
         for string in strings:
-            self.show_a.insert('insert',string + '\n')
-        
-
-
+            self.show_a.insert('insert', string + '\n')
 
     def open_file(self):
+        # set()及delete()将所有文本清零
         self.name.set('')
         self.get_c.set('')
         self.get_w.set('')
         self.get_l.set('')
-        self.show_a.delete('1.0','end')
-        self.text.delete('1.0','end')
+        self.show_a.delete('1.0', 'end')
+        self.text.delete('1.0', 'end')
+
+        # 获取文件名
         self.file_name = filedialog.askopenfilename()
-        self.name.set(self.file_name.split(r'/')[-1])  
-        file = open(self.file_name,'r',encoding= 'utf-8')
+
+        self.name.set(self.file_name.split(r'/')[-1])
+
+        file = open(self.file_name, 'r', encoding='utf-8')
         for line in file.readlines():
-            self.text.insert('insert',line)
+            self.text.insert('insert', line)
 
 
 def handle_files(father_path, all_files, all_path, word):
@@ -153,67 +174,80 @@ def handle_files(father_path, all_files, all_path, word):
         if os.path.isdir(cur_path):
             handle_files(cur_path, all_files, all_path, word)
         elif os.path.isfile(cur_path):
-            if reg.search(file) != None:
+            if reg.search(file):
                 all_path.append(cur_path)
                 all_files.append(file)
 
 
-
-
 class Order():
 
-    
-    def base_orders(self,w_c,words):
+    def base_orders(self, w_c, words):
         # 处理命令
         if '-c' in words:
-            w_c.char_count()
+            print(w_c.char_count())
         if '-w' in words:
-            w_c.words_count()
+            print(w_c.words_count())
         if '-l' in words:
-            w_c.lines_count()
+            print(w_c.lines_count())
         if '-a' in words:
-            w_c.more_information()
-        
-    def senior_orders(self,words):
-        # 处理-s命令
-        
+            for string in w_c.more_information():
+                print(string)
+
+    def senior_orders(self, words):
+        # 处理-s命令，当不存在-c命令时执行base_order
+
         if '-s' in words:
             i = 0
             all_files = []
             all_path = []
+            # word[-1]获取文件名并将通配符替换成正则表达式
             word = words[-1]
+            word = word.replace('.', '\.')
             word = word.replace('*', '.*')
             word = word.replace('?', '.?')
+            # 获取当前文件的父目录
             father_path = os.path.split(os.path.abspath('wc.py'))[0]
+            # 递归获取匹配文件名
             handle_files(father_path, all_files, all_path, word)
             for file in all_files:
                 print(file + ':')
                 w_c = text_count(all_path[i])
-                self.base_orders(w_c,words)
+                self.base_orders(w_c, words)
                 print()
                 i += 1
         else:
-            self.w_c = text_count(words[-1])
-            self.base_orders(self.w_c, words)
+            w_c = text_count(words[-1])
+            self.base_orders(w_c, words)
             print()
 
-    def gui_order(self,words):
+    def gui_order(self, words):
+        # gui界面处理文件
         if '-x' in words:
             win = tkinter.Tk()
-            win.title('可视化文件夹遍历')
+            win.title('WC')
             win.geometry('600x400')
             inwindow = infor_windows(win)
             win.mainloop()
-        else :
+        else:
             self.senior_orders(words)
 
+def help_order():
+    file_name = open('help.txt', 'r',encoding= 'utf-8')
+    print(file_name.read())
+    print()
 
 def main():
     """主函数"""
+    help_order()
+
     # 存储参数
-    words = input("请输入命令和文件路径:").split()
-    order = Order()
-    order.gui_order(words)
+    while True:
+        words = input("请输入命令和文件路径:").split()
+        if '-h' in words:
+            help_order()
+        else :
+            order = Order()
+            order.gui_order(words)
 
 
 if __name__ == "__main__":
